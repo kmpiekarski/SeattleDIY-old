17b6cdd2a95fa5f7776da823f686e6bb3b027c15
slashedtires <kenneth.m.piekarski@gmail.com>
2013-12-31 10:43:48 -0800

    added DB files

---
+++ SeattleDIY phpMyAdmin DB/seattled.sql
@@ -0,0 +1,22 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Dec 30, 2010 at 09:25 PM
+-- Server version: 5.0.91
+-- PHP Version: 5.2.9
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `seattled`
+--
+CREATE DATABASE `seattled` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `seattled`;
